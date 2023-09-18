28baa4caec182ae1c5211bee37920b65017d4fa6
Gustavo <gstvrrds@gmail.com>
2023-09-18 20:31:08 -0300

    fim da segunda aula

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.engine('html',require('ejs').renderFile);//Seta a engine do ejs para renderizar html
-app.set('view engine', 'html');//seta a view engine para html
-app.use('/public', express.static(path.join(__dirname, 'public')));//diz que tudo q é statico vai ficar na pagina publica
-app.set('views', path.join(__dirname,'views'));//diz onde estão as views
+app.engine("html", require("ejs").renderFile); //Seta a engine do ejs para renderizar html
+app.set("view engine", "html"); //seta a view engine para html
+app.use("/public", express.static(path.join(__dirname, "public"))); //diz que tudo q é statico vai ficar na pagina publica
+app.set("views", path.join(__dirname, "views")); //diz onde estão as views
 
-app.get('/',(req,res)=>{
+var tarefas = ['Arrumar o quarto', 'Comprar no supercado'];
 
-  res.render('index',{nome:'gustavo'}); //objeto enviando dados parra o index.html
 
+app.get("/", (req, res) => {
+  res.render("index", { tarefasList:tarefas }); //objeto enviando dados parra o index.html
+});
+
+app.get('/deletar/:id',(req, res)=>{
+    tarefas = tarefas.filter(function(val,index){
+        if(index!= req.params.id){
+            return val;
+        }
+    })
+    res.render('index',{tarefasList:tarefas})
 })
 
-app.listen(5000 , ()=>{
-    console.log('server rodando!');
-})
+app.listen(5000, () => {
+  console.log("server rodando!");
+});
